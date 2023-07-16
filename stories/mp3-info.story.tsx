0a3a47c791d7b78832389b6bb5f73ea017a23394
risonakamo <hmykanae@gmail.com>
2023-07-16 15:33:43 -0400

    complete mp3 info component

--- stories/mp3-info.story.tsx
+++ stories/mp3-info.story.tsx
@@ -2,17 +2,33 @@ import type {Meta,StoryObj} from "@storybook/react";
 
 import {Mp3Info,Mp3InfoProps} from "components/mp3-info/mp3-info";
 
+import {TestCover1} from "lib/sample-img";
+
 type Story=StoryObj<Mp3InfoProps>;
 
 const meta:Meta<Mp3InfoProps>={
   title:"mp3 info",
   component:Mp3Info,
   args:{
+    title:"title of thing",
+    folder:"folder location",
+    duration:"03:04",
 
+    coverImg:TestCover1
   }
 };
 export default meta;
 
 export const normal:Story={
 
+};
+
+export const normal2:Story={
+  args:{
+    title:"01.どきどきアイデアをよろしく!.mp3",
+    folder:"Do It Yourself!! THEME SONGS mp3",
+    duration:"03:56",
+
+    coverImg:TestCover1
+  }
 };
--- web/components/mp3-info/mp3-info.tsx
+++ web/components/mp3-info/mp3-info.tsx
@@ -1,10 +1,12 @@
-import {TestCover1} from "lib/sample-img";
-
 import "./mp3-info.less";
 
 export interface Mp3InfoProps
 {
+  title:string
+  folder:string
+  duration:string
 
+  coverImg:string
 }
 
 export function Mp3Info(props:Mp3InfoProps):JSX.Element
@@ -12,13 +14,13 @@ export function Mp3Info(props:Mp3InfoProps):JSX.Element
   return <div className="mp3-info">
     <div className="top">
       <div className="img-zone">
-        <img src={TestCover1}/>
+        <img src={props.coverImg}/>
       </div>
 
       <div className="details">
-        <h1>title of thing</h1>
-        <h2>folder location</h2>
-        <h3>03:04</h3>
+        <h1>{props.title}</h1>
+        <h2>{props.folder}</h2>
+        <h3>{props.duration}</h3>
       </div>
     </div>
 
